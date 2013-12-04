3cd50c831d6363e95d65eb7c1fe980c785fefa9e
bnohad <bnohad@gmail.com>
2013-12-04 15:28:11 +0200

    Creation of a static database

--- A-Smart-Lecturer/src/aglosh2014/appspot/com/Circles.java
+++ A-Smart-Lecturer/src/aglosh2014/appspot/com/Circles.java
@@ -2,13 +2,19 @@ package aglosh2014.appspot.com;
 
 public class Circles {
 	
+	public static final int NUM_OF_COURSES=5;
+	
 	private int circle_id;
 	private String  circle_name;
+	
+	private Courses courses;
 	
 	public Circles(int id , String name)
 	{
 		circle_id = id;
 		circle_name = name;
+		
+		courses = new Courses(NUM_OF_COURSES);
 	}
 	
 	public int get_id()
@@ -21,6 +27,45 @@ public class Circles {
 		return circle_name;
 	}
 	
+	public void add_new_course(String course_name, String course_year, String lecturer, String checker)
+	{
+		courses.add_new_course(course_name, course_year, lecturer, checker);
+	}
 	
+	public int[] get_student_list_in_course(String course_name)
+	{
+		return courses.get_student_list_in_course(course_name);
+	}
+	
+	public void add_new_student_to_course(int id, String course_name)
+	{
+		courses.add_new_student_to_course(id, course_name);
+	}
+	
+	public void set_course_lecturer(String course_name, String lecturer)
+	{
+		courses.set_course_lecturer(course_name, lecturer);
+	}
+	
+	public void set_course_checker(String course_name, String checker)
+	{
+		courses.set_course_checker(course_name, checker);
+	}
+	
+	public String get_course_lecturer_name(String course_name)
+	{
+		return courses.get_course_lecturer_name(course_name);
+	}
+	
+	public String get_course_checker_name(String course_name)
+	{
+		return courses.get_course_checker_name(course_name);
+	}
+	
+	public String get_courses_in_circle()
+	{
+		return courses.get_courses_list();
+	}
+		
 
 }
--- A-Smart-Lecturer/src/aglosh2014/appspot/com/Courses.java
+++ A-Smart-Lecturer/src/aglosh2014/appspot/com/Courses.java
@@ -5,7 +5,7 @@ public class Courses {
 //this class represent a list of courses & a course object
  static final int NUM_OF_STUDENTS_IN_COURSE=20;
 	
-	static public int new_course_id; //next course to be created
+	public int new_course_id; //next course to be created
 	public Course Courses_table[]; //list of courses
 	
 	public Courses(int num_of_courses) //creates a new list of courses the size of n
@@ -17,7 +17,7 @@ public class Courses {
 		new_course_id=0;
 	}
 	
-	public int add_new_course(String course_name, String course_year) //add new course to list
+	public int add_new_course(String course_name, String course_year, String lecturer, String checker) //add new course to list
 	{
 		if (new_course_id>=Courses_table.length)
 		{
@@ -27,7 +27,7 @@ public class Courses {
 		
 		int temp=new_course_id; //course id
 		
-		Courses_table[new_course_id]=new Course(course_name, course_year); //create new course
+		Courses_table[new_course_id]=new Course(course_name, course_year, lecturer, checker); //create new course
 		
 		if(Courses_table[temp]==null) //if not created
 		{
@@ -38,9 +38,125 @@ public class Courses {
 		return Courses_table[new_course_id-1].course_id; //return new course id
 	}
 	
+	public String get_courses_list()
+	{
+		if(new_course_id==0)
+			return null;
+		
+		String course_list="";
+		
+		for(int i=0; i<new_course_id; i++)
+			course_list+=get_course_name(i) + ",";
+		
+		return course_list;
+			
+	}
+	
 	public String get_course_name(int id)
 	{
 		return Courses_table[id].get_course_name(id);
+	}
+	
+	public int get_course_id(String course_name)
+	{
+		for(int i=0; i<new_course_id; i++)
+		{
+			if(Courses_table[i].course_name.equals(course_name))
+				return i;
+		}
+		
+		return -1;
+	}
+	
+	public int[] get_student_list_in_course(String course_name)
+	{
+		if(course_name.isEmpty())
+			return null;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return null;
+		
+		return Courses_table[course_id].get_students_list();
+	}
+	
+	public void add_new_student_to_course(int id, String course_name)
+	{
+		if(course_name.isEmpty() || id<=0)
+			return;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return;
+		
+		Courses_table[course_id].add_student_to_course(id);
+	}
+	
+	public void set_course_lecturer(String course_name, String lecturer)
+	{
+		if(course_name.isEmpty() || lecturer.isEmpty())
+			return;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return;
+		
+		Courses_table[course_id].set_course_lecturer(lecturer);
+	}
+	
+	public void set_course_checker(String course_name, String checker)
+	{
+		if(course_name.isEmpty() || checker.isEmpty())
+			return;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return;
+		
+		Courses_table[course_id].set_course_checker(checker);
+	}
+	
+	public String get_course_lecturer_name(String course_name)
+	{
+		if(course_name.isEmpty())
+			return null;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return null;
+		
+		return Courses_table[course_id].get_course_lecturer();
+	}
+	
+	public String get_course_checker_name(String course_name)
+	{
+		if(course_name.isEmpty())
+			return null;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return null;
+		
+		return Courses_table[course_id].get_course_checker();
+	}
+	
+	public String get_course_year(String course_name)
+	{
+		if(course_name.isEmpty())
+			return null;
+		
+		int course_id=get_course_id(course_name);
+		
+		if(course_id<0)
+			return null;
+		
+		return Courses_table[course_id].get_course_year();
 	}
 	
 	public class Course {
@@ -48,17 +164,45 @@ public class Courses {
 		private int course_id;
 		private String course_year;
 		private int students_in_course[];
+		private String lecturer, checker;
 		
-		public Course(String course_name, String year)
+		public Course(String course_name, String year, String lecturer, String checker)
 		{
 			if (course_name.equals("") || year.equals(""))
 				return;
 			
+			this.set_course_lecturer(lecturer);
+			this.set_course_checker(checker);
 			this.course_name=course_name;
 			this.course_id=new_course_id++;
 			this.course_year=year;
 			
 			students_in_course=new int[NUM_OF_STUDENTS_IN_COURSE];
+		}
+		
+		public String get_course_year()
+		{
+			return course_year;
+		}
+		
+		public void set_course_lecturer(String name)
+		{
+			lecturer=name;
+		}
+		
+		public void set_course_checker(String name)
+		{
+			checker=name;
+		}
+		
+		public String get_course_lecturer()
+		{
+			return lecturer;
+		}
+		
+		public String get_course_checker()
+		{
+			return checker;
 		}
 		
 		public String get_course_name(int course_id)
